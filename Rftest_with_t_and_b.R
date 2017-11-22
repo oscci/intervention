368d2c91631cfc13405a5fd37ca97b5c94966055
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2017-11-22 05:59:18 +0000

    Based on Rftest, but now generates all 3 expts from same base data and adds 2nd run  where scores are scaled. Shows how b differs for raw and scaled

---
+++ Rftest_with_t_and_b.R
@@ -0,0 +1,125 @@
+#Demonstration of how F-test reflects group effect
+#D.V.M. Bishop, started 20th Nov 2017
+#Thanks to Jan Vanhove for suggesting modification to plots to show mean 
+
+library(doBy) #package to make it easy to get tables of means etc
+library(beeswarm) #nice plotting package
+#if you haven't previously installed doBy and beeswarm, do that first
+#via Tools|Install packages. You only need do this once.
+options(scipen=999)#turn off scientific notation
+set.seed(5) #to ensure same values generated on each run - change to any other number for different results
+
+#create a set of values to simulate for true group differences in SD units
+truediff<-c(.1,.5,1.25) #here we have 3 values; c stands for cocatenate so we get 3 values
+#in a vector; we can refer to them as truediff[1], truediff[2] and truediff[3]
+truelabel<-c('A','B','C') #to label the plots for each run
+
+nsims<-length(truediff)
+bigsummary<-data.frame(matrix(NA,nrow=nsims*2,ncol=13))
+colnames(bigsummary)<-c('Simulation','N.per.gp','Gp1.mean','Gp1.sd','Gp2.mean','Gp2.sd',
+                        'SSB','SSW','F','p','b','b.se','t')
+
+myn<-24 #N per group
+
+#set up data frame to hold simulated raw and z data for 3 expts
+alldat<-data.frame(matrix(NA,nrow=2*myn,ncol=7)) #col 1 for group, col 2-4 for raw, col 5-7 for z
+colnames(alldat)<-c('Group','A','B','C','z.A','z.B','z.C')
+
+alldat$Group<-c(rep(1,myn),rep(2,myn))#generate group IDs for equal sized groups 1 and 2
+
+alldat$A<-rnorm(myn*2) #generate a set of random numbers in first run through
+range1<-1:myn
+range2<-(myn+1):(2*myn)
+for (i in 2:4){
+alldat[range1,i]<-alldat$A[range1]
+alldat[range2,i]<-alldat$A[range2]+truediff[i-1]
+}
+#scale so that variances differ rather than means
+for (i in 2:4){
+alldat[,i]<-alldat[,i]/(i-1)
+#scale to give arbitrary mean 40 and SD 10 for A
+alldat[,i]<-40+(alldat[,i]*10)
+alldat[,(i+3)]<-scale(alldat[,i])
+}
+
+
+for (j in 1:2){#do plots first for raw data, then for z-scores
+  #set up to save plots
+  pngname<-'beeswarm_fratio_raw.png'
+  if (j==2){pngname<-'beeswarm_fratio_z.png'}
+  png(pngname,width=600,height=250)
+  par(mfrow=c(1,nsims)) #one row and nsims columns for plot output
+  
+#alldat #uncomment this to look at the dataframe if you like
+
+for (i in 1:3){
+  #select the column for this expt (i) and this j (raw/z)
+  myindex<-(i+1)+(j-1)*3
+  mynum<-alldat[,myindex]
+myfit <- aov(mynum ~ alldat$Group) #run Anova to test if groups differ
+if(i==1){myfit1<-myfit}
+
+#show this is equivalent to ttest; F is just t squared
+t.test(mynum~alldat$Group,var.equal=TRUE)
+
+#show this is equivalent to test of regression coefficient, b
+bfit<-lm(formula = mynum ~ alldat$Group)
+
+#summary(myfit) #uncomment to see Anova table
+# We are going to compute F-ratio in steps below, but this gives sanity check
+# F-ratio should agree with the one in this summary
+
+#To understand how F-test works, compute F-ratio by formula
+#see https://www.stat.auckland.ac.nz/~wild/ChanceEnc/Ch10.byhand.pdf
+
+#First we'll get means, sds and variances for each group
+#Note: we don't need sds, but they are more familiar when reporting data.
+#The variance is just the sd squared
+alldat$temp<-mynum #create temp column so can use summaryBy, which requires ref to data.frame
+mysum<-summaryBy(temp ~ Group, data=alldat,
+          FUN = function(x) { c(m = mean(x), s = sd(x),v=var(x)) } )
+colnames(mysum)<-c('group','mean','sd','var')
+#mysum #uncomment this line to see means, sd and var by group
+ssB<-var(mysum$mean)*myn #between subjects SS is variance of means x group size
+ssW<-mean(mysum$var) #within subjects SS is average variance per group
+myF<-ssB/ssW #F is ratio of the two
+
+#add results to summary table
+index2<-myindex-1
+bigsummary[index2,1]<-myindex-1
+bigsummary[index2,2]<-myn
+bigsummary[index2,3]<-mysum[1,2]
+bigsummary[index2,4]<-mysum[1,3]
+bigsummary[index2,5]<-mysum[2,2]
+bigsummary[index2,6]<-mysum[2,3]
+bigsummary[index2,7]<-ssB
+bigsummary[index2,8]<-ssW
+bigsummary[index2,9]<-myF
+bigsummary[index2,10]<-pf(myF,1,(myn*2-2),lower.tail=F)
+bigsummary[index2,11:13]<-summary(bfit)$coefficients[2,1:3]
+
+#Show the plot with the stats
+myylab<-''
+if (i==1){myylab<-'Outcome'}
+alldat$Group<-as.factor(alldat$Group)
+levels(alldat$Group)<-c('Control','Intervention')
+my.ylim<-c(0,70) #y axis limits
+texty<-67 #vertical location of text placement for F value
+#adjust these values for z-score plot
+if (j==2){my.ylim<-c(-3,3)
+   texty<-2.7}
+beeswarm(mynum~alldat$Group ,xlab=truelabel[i],ylab=myylab,
+         col='red',pch=16,ylim=my.ylim,cex.axis=1.5,cex.lab=2)
+segments(x0 = 0.7, x1 = 1.3,
+         y0 = bigsummary[index2, 3], 
+         lty = 1, lwd = 2,col='black')
+segments(x0 = 1.7, x1 = 2.3,
+         y0 = bigsummary[index2, 5], 
+         lty = 1, lwd = 2,col='black')
+
+Fbit<-paste0('F = ',round(myF,2))
+text(1.3,texty,Fbit,cex=1.5)
+
+}
+dev.off() #stop printing to png
+}
